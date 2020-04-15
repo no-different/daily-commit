6ef7c23efea7e819b940c97e4a71966639be4199
yjjung <yjjung@yjjungui-MacBookPro.local>
2020-04-15 22:58:51 +0900

    Initial commit

---
+++ src/main/java/com/lipsguswn/dailycommit/DailyCommitApplication.java
@@ -0,0 +1,13 @@
+package com.lipsguswn.dailycommit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyCommitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyCommitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lipsguswn/dailycommit/DailyCommitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lipsguswn.dailycommit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyCommitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
